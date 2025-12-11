9237a5d94e85ab68a73761cd42229b1ade25939d
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-12-11 22:46:34 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,18 +1,11 @@
+# Exercise 1
 def fibonacci(n):
     pass
- 
-def count_digits(n):
-    for i in range(10):
-        print(i)
-    if n < 10:
-        return 1
-    else:
-        return 1 + count_digits(n // 10)
 
+# Exercise 2
+def count_digits(n):
+    pass
+
+# Exercise 3
 def sum_digits(n):
-    for i in range(10):
-        print(i)
-    if n == 0:
-        return 0
-    else:
-        return n % 10 + sum_digits(n // 10)
+    pass
